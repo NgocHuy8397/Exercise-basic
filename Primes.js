b0629e6f6ec85221e7cc8f9385635b08f30c98b6
Huy Nguyen <huy.nguyen@evolus.vn>
2022-11-10 16:32:39 +0700

    Update exercise

--- Primes.js
+++ Primes.js
@@ -4,14 +4,14 @@ function isprime(n){
   }
 
   for(var i = 2; i <= n / 2 ; i++){
-    if(n % 2 == 0){
+    if(n % i == 0){
         return false;
     }
   }
   return true;
 }
 
-var array = [0,1,2,3,4,5,6,7,8,10];
+var array = [0,1,2,3,4,5,6,7,8,9,10];
 var sum = 0;
 for (var i = 0; i < array.length; i++){
   if (isprime(array[i]) == 1){
